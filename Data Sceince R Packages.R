ef3588067078edee36588154d33696830a017375
Cornelius Tanui <46952626+corneliustanui@users.noreply.github.com>
2019-12-20 19:22:28 +0300

    Add files via upload

---
+++ Data Sceince R Packages.R
@@ -0,0 +1,37 @@
+# DATA SCIENCE PAKCAGES
+#install.packages("lattice")
+#install.packages("DataExplorer")
+#install.packages("dplyr")
+#install.packages("esquisse")
+#install.packages("caret")
+#install.packages("janitor")
+#install.packages("rpart")
+#install.packages("prophet")
+#install.packages("plotly")
+#install.packages("mlr")
+install.packages("quanteda.dictionaries")
+#install.packages("DT")
+#install.packages("Rcrawler")
+#install.packages("rmarkdown")
+#install.packages("leaflet")
+#install.packages("infer")
+#install.packages("parsnip")
+#install.packages("ranger")
+#install.packages("purrr")
+#install.packages("rtweet")
+#install.packages("tensorflow")
+#install.packages("keras")
+#install.packages("reticulate")
+#install.packages("RWeka")
+#install.packages("tidytext")
+
+#_________________________________
+library(devtools)
+install_github("happyrabbit/DataScienceR")
+install_github("kbenoit/quanteda.dictionaries") 
+library(DataScienceR)
+library(quanteda.dictionaries)
+#_________________________________
+#Please download and install Rtools custom from http://cran.r-project.org/bin/windows/Rtools/.
+
+
